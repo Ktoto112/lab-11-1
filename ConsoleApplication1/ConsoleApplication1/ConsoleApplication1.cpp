824a3c61b9d36d9ff05f0857acc1b78a18bf234f
Ktoto112 <19.12.2006sasha@gmail.com>
2025-04-11 20:17:28 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;          
+    double* ptrA, * ptrB;  
+
+   
+    cout << "Enter a value for a: ";
+    cin >> a;
+    cout << "Enter a value for b: ";
+    cin >> b;
+
+  
+    ptrA = &a;
+    ptrB = &b;
+
+   
+    cout << "\nInitial values:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+   
+    if (*ptrA > *ptrB) {
+        *ptrA += 7;  
+        *ptrB -= 3;  
+    }
+    else {
+        *ptrB += 7;  
+        *ptrA -= 3; 
+    }
+
+    
+    cout << "\nAfter the changes:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
